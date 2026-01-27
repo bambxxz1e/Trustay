0cf91883fe9523f0c52f2676f7606e4b3b601704
윤성연 <s2427@e-mirim.hs.kr>
2026-01-27 11:43:21 +0900

    홈 화면 조금 구현

---
+++ front/lib/models/house_dummy.dart
@@ -0,0 +1,25 @@
+class HouseDummy {
+  final int id;
+  final String title;
+  final String address;
+  final String houseType;
+  final String approvalStatus;
+  final List<String> imageUrls;
+  final int price;
+  final int beds;
+  final int baths;
+  final int people;
+
+  HouseDummy({
+    required this.id,
+    required this.title,
+    required this.address,
+    required this.houseType,
+    required this.approvalStatus,
+    required this.imageUrls,
+    required this.price,
+    required this.beds,
+    required this.baths,
+    required this.people,
+  });
+}
