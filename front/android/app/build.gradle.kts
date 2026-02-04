cfbcdcb204ab4a8e793b1ffc01145c0f1107618c
윤성연 <s2427@e-mirim.hs.kr>
2026-02-04 13:29:32 +0900

    mapbox 버리고 openstreetmap으로 갈아타기

--- front/android/app/build.gradle.kts
+++ front/android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
 }
@@ -17,24 +16,23 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "17" // 이거만 쓰면 됨
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.front"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        // Mapbox 토큰
+        manifestPlaceholders["MAPBOX_ACCESS_TOKEN"] =
+            project.findProperty("MAPBOX_ACCESS_TOKEN")?.toString() ?: ""
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
